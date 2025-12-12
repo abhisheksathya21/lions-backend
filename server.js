52ac92a93b526aa84bdd70ef1baa0b0867e687a5
Abhishek <abhisheksathya56@gmail.com>
2025-12-12 18:34:14 +0530

    changes in server

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
+
 
 // API Routes
 app.use("/api/admin", adminRoutes);
